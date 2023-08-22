628bda8595f8df937e20d2750879c0d9014f1c23
Noflare <80207554+Noflare@users.noreply.github.com>
2023-08-22 09:29:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,52 @@
 import asyncio
 from aiosmtpd.controller import Controller
 from email import message_from_bytes
+import re
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+latest_link = ""
 
 class MailHandler:
     async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
-        # This part checks whether the mail coming in is actually
-        # coming in to where it should, as in besavvy.fr which
-        # is my domain. Technically not fully necessary but nice
-        # to have.
-        if not address.endswith('@besavvy.fr'):
-            print("Address does not end with @besavvy.fr")
-            return '550 not relaying to that domain'
-        envelope.rcpt_tos.append(address)
-        return '250 OK'
+        # ... (votre code de gestion des emails)
+        pass
+
     async def handle_DATA(self, server, session, envelope):
-        # This one if the sender, so if it was sent to
-        # someone@gmail.com mail_from will be that
-        print('Message from %s' % envelope.mail_from)
-
-        # This is a string array of who it was sent to
-        # like ['bob@xxxx.com','jeff@xxxx.com']
-        # it can have a length of 1 if theres only 1 
-        # person it was sent to
-        print('Message for %s' % envelope.rcpt_tos)
-
-        # If you want to print EVERYTHING in the mail
-        # including useless information, use this
-        #print('Message data:\n')
-        #for ln in envelope.content.decode('utf8', errors='replace').splitlines():
-        #    print(f'> {ln}'.strip())
-
-
-        # This code is to just read the text part of the email
-        # in other words the useful part that we actually, as in
-        # the actual text inside the mail
         plain_text_part = None
         email_message = message_from_bytes(envelope.content)
         for part in email_message.walk():
             if part.get_content_type() == 'text/plain':
                 plain_text_part = part.get_payload(decode=True).decode('utf-8')
                 break
+
         if plain_text_part:
-            # Do something with the plain text part
-            print("Plain text content:")
-            print(plain_text_part)
+            # Extract links using regular expression
+            links = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', plain_text_part)
 
+            if links:
+                global latest_link
+                latest_link = links[-1]  # Update with the latest link
+                socketio.emit('latest_link', {"latest_link": latest_link})
 
-        # This is to finish the mail and see that it finished
-        # it can be removed, just visual
         print()
         print('End of message')
         return '250 Message accepted for delivery'
 
-# Here you start the actual server, hostname is your PRIVATE ipv4, and port has to be 25
-# Change it to your actual local ipv4 or use localhost
-controller = Controller(MailHandler(), hostname='localhost', port=25)
+controller = Controller(MailHandler(), hostname='10.0.0.4', port=25)
 controller.start()
 asyncio.get_event_loop().run_forever()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    emit('latest_link', {"latest_link": latest_link})
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
