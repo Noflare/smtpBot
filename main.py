f6f563c7e53b31d7558d93a6dd080bd69c598cdb
Noflare <80207554+Noflare@users.noreply.github.com>
2023-08-22 09:52:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import asyncio
 from aiosmtpd.controller import Controller
 from email import message_from_bytes
 import re
-import multiprocessing
-from multiprocessing import Process
 
 class MailHandler:
     async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
-        # ... (votre code de gestion des emails)
-        pass
-
+        if not address.endswith('@besavvy.fr'):
+            print("Address does not end with @besavvy.fr")
+            return '550 not relaying to that domain'
+        envelope.rcpt_tos.append(address)
+        return '250 OK'
+    
     async def handle_DATA(self, server, session, envelope):
         plain_text_part = None
         email_message = message_from_bytes(envelope.content)
@@ -21,23 +22,14 @@ class MailHandler:
         if plain_text_part:
             # Extract links using regular expression
             links = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', plain_text_part)
-
-            if links:
-                latest_link = links[-1]  # Update the latest link
-
-                # Save the latest link to a file
-                with open('links.txt', 'w') as file:
-                    file.write(latest_link)
+            for link in links:
+                print("Extracted link:")
+                print(link)
 
         print()
         print('End of message')
         return '250 Message accepted for delivery'
 
-def start_smtp_server():
-    controller = Controller(MailHandler(), hostname='10.0.0.4', port=25)
-    controller.start()
-    asyncio.get_event_loop().run_forever()
-
-if __name__ == '__main__':
-    smtp_process = Process(target=start_smtp_server)
-    smtp_process.start()
+controller = Controller(MailHandler(), hostname='10.0.0.4', port=25)
+controller.start()
+asyncio.get_event_loop().run_forever()
